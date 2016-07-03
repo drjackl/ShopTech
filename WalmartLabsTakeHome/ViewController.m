0f01aa194221bd1293838880d5703456fbd425c5
Jack Li <drjackl@gmail.com>
2016-07-02 19:06:36 -0700

    Initial Commit

---
+++ WalmartLabsTakeHome/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WalmartLabsTakeHome
+//
+//  Created by Jack Li on 7/2/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
